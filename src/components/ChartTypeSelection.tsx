bcf172dd01562f98a5bc0d99f5aedd87c01ae837
jasonLapina <lapina.jason@gmail.com>
2025-05-11 07:48:40 +0800

    update grid props

--- src/components/ChartTypeSelection.tsx
+++ src/components/ChartTypeSelection.tsx
@@ -1,17 +1,25 @@
-import { Box, Typography, Grid, Card, CardContent, CardMedia, Button } from '@mui/material';
-import { useAppDispatch } from '../redux/hooks';
-import { selectChartType } from '../redux/chartSlice';
-import { ChartType } from '../types/chartTypes';
-import PieChartComponent from './charts/PieChartComponent';
-import BarChartComponent from './charts/BarChartComponent';
-import LineChartComponent from './charts/LineChartComponent';
-import AreaChartComponent from './charts/AreaChartComponent';
-import { 
-  pieChartPreviewData, 
-  barChartPreviewData, 
-  lineChartPreviewData, 
-  areaChartPreviewData 
-} from '../utils/chartPreviewData';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { useAppDispatch } from "../redux/hooks";
+import { selectChartType } from "../redux/chartSlice";
+import { ChartType } from "../types/chartTypes";
+import PieChartComponent from "./charts/PieChartComponent";
+import BarChartComponent from "./charts/BarChartComponent";
+import LineChartComponent from "./charts/LineChartComponent";
+import AreaChartComponent from "./charts/AreaChartComponent";
+import {
+  areaChartPreviewData,
+  barChartPreviewData,
+  lineChartPreviewData,
+  pieChartPreviewData,
+} from "../utils/chartPreviewData";
 
 const ChartTypeSelection = () => {
   const dispatch = useAppDispatch();
@@ -21,34 +29,38 @@ const ChartTypeSelection = () => {
   };
 
   return (
-    <Box sx={{ maxWidth: 1200, margin: '0 auto', padding: 3 }}>
+    <Box sx={{ maxWidth: 1200, margin: "0 auto", padding: 3 }}>
       <Typography variant="h4" component="h1" gutterBottom align="center">
         Select Chart Type
       </Typography>
       <Typography variant="body1" paragraph align="center">
-        Choose the type of chart you want to create. A preview is shown for each chart type.
+        Choose the type of chart you want to create. A preview is shown for each
+        chart type.
       </Typography>
 
       <Grid container spacing={4} justifyContent="center">
         {/* Pie Chart Card */}
-        <Grid item xs={12} sm={6} md={3}>
-          <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+          <Card
+            sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+          >
             <CardContent sx={{ flexGrow: 1 }}>
               <Typography gutterBottom variant="h5" component="h2">
                 Pie Chart
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                Good for showing proportions of a whole, like spending by category.
+                Good for showing proportions of a whole, like spending by
+                category.
               </Typography>
             </CardContent>
             <CardMedia sx={{ height: 200, padding: 2 }}>
               <PieChartComponent data={pieChartPreviewData} isPreview={true} />
             </CardMedia>
             <CardContent>
-              <Button 
-                fullWidth 
-                variant="contained" 
-                onClick={() => handleSelectChart('pie')}
+              <Button
+                fullWidth
+                variant="contained"
+                onClick={() => handleSelectChart("pie")}
               >
                 Select Pie Chart
               </Button>
@@ -57,8 +69,10 @@ const ChartTypeSelection = () => {
         </Grid>
 
         {/* Bar Chart Card */}
-        <Grid item xs={12} sm={6} md={3}>
-          <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+          <Card
+            sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+          >
             <CardContent sx={{ flexGrow: 1 }}>
               <Typography gutterBottom variant="h5" component="h2">
                 Bar Chart
@@ -71,10 +85,10 @@ const ChartTypeSelection = () => {
               <BarChartComponent data={barChartPreviewData} isPreview={true} />
             </CardMedia>
             <CardContent>
-              <Button 
-                fullWidth 
-                variant="contained" 
-                onClick={() => handleSelectChart('bar')}
+              <Button
+                fullWidth
+                variant="contained"
+                onClick={() => handleSelectChart("bar")}
               >
                 Select Bar Chart
               </Button>
@@ -83,8 +97,10 @@ const ChartTypeSelection = () => {
         </Grid>
 
         {/* Line Chart Card */}
-        <Grid item xs={12} sm={6} md={3}>
-          <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+          <Card
+            sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+          >
             <CardContent sx={{ flexGrow: 1 }}>
               <Typography gutterBottom variant="h5" component="h2">
                 Line Chart
@@ -94,13 +110,16 @@ const ChartTypeSelection = () => {
               </Typography>
             </CardContent>
             <CardMedia sx={{ height: 200, padding: 2 }}>
-              <LineChartComponent data={lineChartPreviewData} isPreview={true} />
+              <LineChartComponent
+                data={lineChartPreviewData}
+                isPreview={true}
+              />
             </CardMedia>
             <CardContent>
-              <Button 
-                fullWidth 
-                variant="contained" 
-                onClick={() => handleSelectChart('line')}
+              <Button
+                fullWidth
+                variant="contained"
+                onClick={() => handleSelectChart("line")}
               >
                 Select Line Chart
               </Button>
@@ -109,8 +128,10 @@ const ChartTypeSelection = () => {
         </Grid>
 
         {/* Area Chart Card */}
-        <Grid item xs={12} sm={6} md={3}>
-          <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+          <Card
+            sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+          >
             <CardContent sx={{ flexGrow: 1 }}>
               <Typography gutterBottom variant="h5" component="h2">
                 Area Chart
@@ -120,13 +141,16 @@ const ChartTypeSelection = () => {
               </Typography>
             </CardContent>
             <CardMedia sx={{ height: 200, padding: 2 }}>
-              <AreaChartComponent data={areaChartPreviewData} isPreview={true} />
+              <AreaChartComponent
+                data={areaChartPreviewData}
+                isPreview={true}
+              />
             </CardMedia>
             <CardContent>
-              <Button 
-                fullWidth 
-                variant="contained" 
-                onClick={() => handleSelectChart('area')}
+              <Button
+                fullWidth
+                variant="contained"
+                onClick={() => handleSelectChart("area")}
               >
                 Select Area Chart
               </Button>
